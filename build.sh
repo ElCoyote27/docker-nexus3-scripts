f1727aa3f0fb49d26dc651a59d5f721cd3b36b93
Vincent S. Cojot <vcojot@redhat.com>
2025-01-26 20:16:59 +0100

    adjust

--- build.sh
+++ build.sh
@@ -9,11 +9,12 @@ NEXUS_DOCKERFILE=Dockerfile.rh.ubi.java17
 cd ${NEXUS_GIT_DIR} || exit 127
 
 git reset --hard
+git checkout main
 git pull
 git checkout ${NEXUS_VERSION}
 
 # Workaround
-sed -i -e 's/ gzip//g' ${NEXUS_DOCKERFILE}
+sed -i -e 's@procps shadow-utils@procps shadow-utils gzip@g' ${NEXUS_DOCKERFILE}
 
 # Build
 IMAGE_NAME=krynn-nexus3
